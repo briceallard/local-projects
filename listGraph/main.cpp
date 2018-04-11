b7a475e1935f34ef69d8007458ecfb92f70a6cdb
Brice Allard <briceallard@gmail.com>
2018-04-11 02:52:47 -0500

    Making headway on adding edges to closest vertices (by Distance)

--- listGraph/main.cpp
+++ listGraph/main.cpp
@@ -2,13 +2,13 @@
 #include "distance.h"
 #include "graph.h"
 #include <algorithm>
+#include <utility>
 
+Graph loadGraph(std::string, std::string, std::string);
 int num_of_lines(std::string);
+int search(Graph&, std::string);
 void closestEdges(Graph&);
 void randomEdges(Graph&, int);
-Graph loadGraph(std::string, std::string, std::string);
-Graph loadGraphCSV(std::string, int);
-Graph loadGraphCSV(std::string, std::string, int);
 
 // Test Driver
 int main(int argc, char *argv[])
@@ -115,37 +115,71 @@ int num_of_lines(std::string filename) {
     return numLines;
 }
 
+int search(Graph &G, std::string key) {
+    for (std::vector<int>::size_type i = 0; i != G.vList.size(); i++)
+    {
+        if (G.vList[i]->city == key)
+        {
+            std::cout << G.vList[i]->city << "::" << i << "::"
+                      << G.vList[i]->state << " Found!" << std::endl;
+            return i;
+        }
+    }
+    return -1;
+}
+
 void closestEdges(Graph &G) {
     int edgePerV;
-    std::string start;
+    int index;
+    std::string key;
     double distance;
     latlon from;
     latlon to;
 
-    std::cout << "Center City (Case Sensitive): ";
-    std::cin.ignore();
-    std::getline(std::cin, start);
+    while(1) {
+        std::cout << "Center City (Case Sensitive): ";
+        std::getline(std::cin, key);
 
-    // auto it = std::find(G.vList.begin(), G.vList.end(), start);
-    // if(it == G.vList.end()) {
-    //     std::cout << "Enter a city name that is located in the graph ensuring case sensitive" << std::endl;
-    //     goto beginning;
-    // }
-    // else {
-    //     auto startIndex = std::distance(G.vList.begin(), it);
-    //     std::cout << "CENTER CITY::" << G.vList[startIndex]->city << std::endl;
-    // }
+        index = search(G, key);
 
-
-    std::cout << "Edges per Vertex: ";
-    std::cin >> edgePerV;
-
-    if(std::cin.fail()) {
-        std::cout << "Enter a proper integer!" << std::endl;
-        exit(0);
+        if(index == -1)
+            std::cout << key << " not found!" << std::endl;
+        else if(index != -1)
+            break;
     }
-    else {
-        std::cout << "Digit entered!" << std::endl;
+
+    while(1) {
+        std::cout << "Edges per Vertex: ";
+        std::cin >> edgePerV;
+
+        if(std::cin.fail()) {
+            std::cout << "Enter a proper integer!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else
+            break;
+    }
+
+    std::vector<std::pair<double, int> > closestV;
+
+    for(int i = 0; i < G.vList.size(); i++) {
+        from = G.vList[index]->location;
+        to = G.vList[i]->location;
+        distance = distanceEarth(from.lat, from.lon, to.lat, to.lon);
+        closestV.push_back(std::make_pair(distance, i));
+    }
+
+    std::sort(closestV.begin(), closestV.end());
+
+    for(int i = 1; i <= edgePerV; i++) {
+        std::cout << '[' << closestV[i].first << "::" 
+                    << closestV[i].second << ']' << std::endl;
+
+        index = closestV[i].second;
+
+        std::cout << G.vList[index]->city << "::" << index << "::"
+                << G.vList[index]->state << std::endl;
     }
 }
 
@@ -162,75 +196,4 @@ void randomEdges(Graph &G,int numEdges){
         d = distanceEarth(from.lat,from.lon,to.lat,to.lon);
         G.addEdge(r1,r2,(int)d,true);
     }
-}
-
-/**
- * loadGraphCSV - loads a graph with the given csv
- * Params:
- *     string filename  - filename to open
- * Returns 
- *     graph
- */
-Graph loadGraphCSV(std::string filename, int max = 0)
-{
-    srand(984325);
-    int zip;
-    double lat;
-    double lon;
-    std::string city;
-    std::string state;
-    std::string county;
-
-    strint cityCheck;
-
-    int i=0;
-
-    Graph G;
-
-    std::ifstream file(filename);
-
-    for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
-    {
-        zip = std::stoi((*loop)[0]);
-        if ((*loop)[1].length() > 0)
-        {
-            lat = std::stod((*loop)[1]);
-        }
-        else
-        {
-            lat = 0.0;
-        }
-
-        if ((*loop)[2].length() > 0)
-        {
-            lon = std::stod((*loop)[2]);
-        }
-        else
-        {
-            lon = 0.0;
-        }
-
-        city = (*loop)[3];
-        state = (*loop)[4];
-        county = (*loop)[5];
-
-        
-        if (cityCheck.find(city) == cityCheck.end())
-        {   
-            // Add the city as a key to the map.
-            cityCheck[city] = 0;
-
-            if(state != "PR"){
-                G.addVertex(city, state, lat, lon);
-                i++;
-            }
-        }
-
-        if(i > max && max != 0){
-            return G;
-        }
-        
-    }
-
-    return G;
 }
